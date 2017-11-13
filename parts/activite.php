fe29977ff3b52ca064e4cfeb6e7ed7a2b0533ef4
AVALANCHE !!! <marache.b@gmail.com>
2017-11-13 16:12:46 +0100

    Modification dans la page activité pour recevoir les informations venant de la BDD

--- parts/activite.php
+++ parts/activite.php
@@ -1,8 +1,11 @@
 <article class="col-md-6">
 	<section class="activite">
 	<h4>Nom de l'activité</h4>
-	<h3> La date - Numéto de la salle </h3>
+	<p class="nom"></p>
+	<h3> La date - Numéro de la salle </h3>
+	<p class="num-salle"></p>
 	<p>description de l'activité + nom du coach qui la réalise</p>
+	<p class="description"></p>
 	<button>S'inscrire à cette activité</button>
 	</section>
 </article>
