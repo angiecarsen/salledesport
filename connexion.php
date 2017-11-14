68e08a40d6b7255b459150ff25c03a811de4980a
Théo Chapelot <theo.chapelot@gmail.com>
2017-11-14 15:15:12 +0100

    connexion.php maj
    J'ai gagné quelques lignes

--- connexion.php
+++ connexion.php
@@ -7,13 +7,15 @@ $connecteur = new PDO('mysql:host=localhost;dbname=salle_sport','salledesport','
 
 
 if (!isset($_SESSION['role'])) { //On vérifie que l'on se soit pas connecté 
-	if (isset($_GET['admin'])) {   //On regarde si l'utilisateur veut s'identifier en admin
-		$email = /*$_POST['email']*/ 'joce@lyn.at';
-		$numero = /*$_POST['numero']*/ '000011112222BBBB';
 
-		$info_admin = $connecteur->query('SELECT * FROM coach')->fetchAll(PDO::FETCH_ASSOC);
+	$email = /*$_POST['email']*/ 'joce@lyn.at';
+	$numero = /*$_POST['numero']*/ '000011112222BBBB';
 
-		foreach ($info_admin as $ia) {
+	$info = $connecteur->query('SELECT * FROM coach')->fetchAll(PDO::FETCH_ASSOC);
+
+	if (isset($_GET['admin'])) {   //On regarde si l'utilisateur veut s'identifier en admin	
+
+		foreach ($info as $ia) {
 			if ($ia['email'] == $email) {
 				
 				if ($ia['numeroLicence'] == $numero) {
@@ -28,16 +30,9 @@ if (!isset($_SESSION['role'])) { //On vérifie que l'on se soit pas connecté
 		}
 
 	} else if (isset($_GET['user'])) { //ou en adherent
-		$email = /*$_POST['email']*/ 'bobama@us.gov.com';
-		$numero = /*$_POST['numero']*/ '00148789';
-		
-
-		//On charge tous les utilisateurs et toutes leurs infos dans info_user
-		$info_user = $connecteur->query('SELECT * FROM adherent')->fetchAll(PDO::FETCH_ASSOC);
-
 
 		// Pour chaque utilisateur, on vérifie si l'email entré correspond à un utilisateur puis si le numero entré correspond bien au numéro de l'utilisateur correspondant
-		foreach ($info_user as $iu) {
+		foreach ($info as $iu) {
 			if ($iu['email'] == $email) {
 				
 				if ($iu['numero'] == $numero) {
