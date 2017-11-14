25e215f48726d2aec601d735f3bef929a1379737
Théo Chapelot <theo.chapelot@gmail.com>
2017-11-14 14:48:02 +0100

    connexion.php fini

--- connexion.php
+++ connexion.php
@@ -8,33 +8,57 @@ $connecteur = new PDO('mysql:host=localhost;dbname=salle_sport','salledesport','
 
 if (!isset($_SESSION['role'])) { //On vérifie que l'on se soit pas connecté 
 	if (isset($_GET['admin'])) {   //On regarde si l'utilisateur veut s'identifier en admin
-	
+		$email = /*$_POST['email']*/ 'joce@lyn.at';
+		$numero = /*$_POST['numero']*/ '000011112222BBBB';
 
+		$info_admin = $connecteur->query('SELECT * FROM coach')->fetchAll(PDO::FETCH_ASSOC);
 
-	} else if (isset($_GET['user'])) { //ou en adherent
-		$email = /*$_POST['email']*/ 'jean@pruliere.fr';
-		 
-		$mailbdd = $connecteur->query('SELECT email FROM adherent')->fetchAll(PDO::FETCH_ASSOC);
-		var_dump($mailbdd);
+		foreach ($info_admin as $ia) {
+			if ($ia['email'] == $email) {
+				
+				if ($ia['numeroLicence'] == $numero) {
+					
+					$_SESSION['role'] = 'admin';
+					$_SESSION['prenom'] = $ia['nomComplet'];		
 
-		foreach ($mailbdd as $m) {
-			if ($m['email'] == $email) {
-				echo $email;
-			}
+				} else {
+					echo "Erreur dans le numero";
+				}
+			} 
 		}
 
-		$_SESSION['prenom'] = '06553'; 
- 
-  		echo $_SESSION['prenom'];
+	} else if (isset($_GET['user'])) { //ou en adherent
+		$email = /*$_POST['email']*/ 'bobama@us.gov.com';
+		$numero = /*$_POST['numero']*/ '00148789';
+		
+
+		//On charge tous les utilisateurs et toutes leurs infos dans info_user
+		$info_user = $connecteur->query('SELECT * FROM adherent')->fetchAll(PDO::FETCH_ASSOC);
 
 
+		// Pour chaque utilisateur, on vérifie si l'email entré correspond à un utilisateur puis si le numero entré correspond bien au numéro de l'utilisateur correspondant
+		foreach ($info_user as $iu) {
+			if ($iu['email'] == $email) {
+				
+				if ($iu['numero'] == $numero) {
+					
+					$_SESSION['role'] = 'user';
+					$_SESSION['prenom'] = $iu['prenom'];		
+
+				} else {
+					echo "Erreur dans le numero";
+				}
+			} 
+		}
 	}
    
+echo "Bonjour ".$_SESSION['prenom'];
 
 } else {  //Sinon on déconnecte
   unset($_SESSION['prenom']);
   unset($_SESSION['role']); 
 } 
+
  
 
 	
