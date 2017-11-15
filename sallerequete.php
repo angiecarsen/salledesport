d69d28d98870c3f34caaf18f2afbf657516dadd5
Celine <celine.beber@gmail.com>
2017-11-15 14:50:00 +0100

    u_u

--- sallerequete.php
+++ sallerequete.php
@@ -1,4 +1,4 @@
-<?php $connecteur = new PDO('mysql:host=localhost;dbname=salledesport','salledesport','hopeforce3');
+<?php $connecteur = new PDO('mysql:host=localhost;dbname=salle_de_sport','salledesport','hopeforce3');
           $resultatsalle = $connecteur->query('SELECT * FROM salle ;')->fetchAll(PDO::FETCH_ASSOC); 
                     
 
