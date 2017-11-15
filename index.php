c1b407ec14f31c6b7b14223bd02a86dc5383a934
Unknown <cfjacquet@hotmail.fr>
2017-11-15 12:30:18 +0100

    index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+	
 		<title>Complexe Sportif</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -13,7 +14,7 @@
 	$page = 'accueil';
 	require_once('parts/nav.php');
 ?>
-<h3>Dijon sport</h3>
+<h3>Complexe Sportif</h3>
 
 <?php require_once('parts/main.php'); ?>
 
