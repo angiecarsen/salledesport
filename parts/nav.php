471b6680fe0bb854368bb71939c80b4b7810ad75
Théo Chapelot <theo.chapelot@gmail.com>
2017-11-15 10:04:21 +0100

    connexion via identifiant et numéro

--- parts/nav.php
+++ parts/nav.php
@@ -46,17 +46,17 @@
 				    <div class="modal-content">
 				      <p>Connexion</p>
 				         
-				      		<form class="form-horizontal" action="connexion.php?admin=" method="post">
+				      		<form class="form-horizontal" action="connexion.php" method="post">
 				      		  <div class="form-group">
 				      		    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 				      		    <div class="col-sm-10">
-				      		      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+				      		      <input type="email" class="form-control" id="inputEmail3" name="email" placeholder="Email">
 				      		    </div>
 				      		  </div>
 				      		  <div class="form-group">
 				      		    <label for="inputPassword3" class="col-sm-2 control-label">Numéro</label>
 				      		    <div class="col-sm-10">
-				      		      <input type="password" class="form-control" id="inputPassword3" placeholder="numero">
+				      		      <input type="password" class="form-control" id="inputPassword3" name="numero" placeholder="numero">
 				      		    </div>
 				      		  </div>
 				      		  <div class="form-group">
