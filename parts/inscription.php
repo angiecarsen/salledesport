262af94a7e240572eaedcce7346436f0d7e483d0
AVALANCHE !!! <marache.b@gmail.com>
2017-11-16 10:52:53 +0100

    Formulaire inscription

--- parts/inscription.php
+++ parts/inscription.php
@@ -1,18 +1,18 @@
- <form>
+ <form method="post" action="">
   <div class="form-group">
 	<label for="prenom">Prénom:</label>
-    <input type="texte" class="form-control" id="prenom" required>
+    <input type="texte" class="form-control" id="prenom" name="prenom" required>
 	
 	<label for="nom">Nom:</label>
-    <input type="texte" class="form-control" id="nom" required>
+    <input type="texte" class="form-control" id="nom" name="nom" required>
 	
     <label for="email">Email:</label>
-    <input type="email" class="form-control" id="email" required>
+    <input type="email" class="form-control" id="email" name="email" required>
   
 	<label for="num">Numéro de téléphone:</label>
-    <input type="texte" class="form-control" id="num" required>
+    <input type="texte" class="form-control" id="num" name="num" required>
     
 	</div>
   <button type="submit" formaction="SQL/inscri.php" class="btn btn-default">Submit</button>
   
-</form> 
+</form>
